0b736060c154ffad38ff01c62e6e4c43e0c5d72f
cqx <cqx@163.com>
2023-10-30 15:53:19 +0800

    最长公共前缀答案

--- 最长公共前缀/Solution.java
+++ 最长公共前缀/Solution.java
@@ -91,3 +91,52 @@ class Solution {
 
 
 }
+
+答案1
+        String substring(int beginIndex, int endIndex) ：返回一个新字符串，它是此字符串从beginIndex开始截取到endIndex(不包含)的一个子字符串。
+substring搭配startwith，以第一个字符串为准全拼为准，如果不匹配前缩一位
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0)return "";
+        //公共前缀比所有字符串都短，随便选一个先
+        String s=strs[0];
+        for (String string : strs) {
+            while(!string.startsWith(s)){
+                if(s.length()==0)return "";
+                //公共前缀不匹配就让它变短！
+                s=s.substring(0,s.length()-1);
+            }
+        }
+        return s;
+    }
+}
+
+答案2递归
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];//对比基准
+        int count = strs.length;
+        for (int i = 1; i < count; i++) {
+            prefix = longestCommonPrefix(prefix, strs[i]);
+            if (prefix.length() == 0) {
+                break;
+            }
+        }
+        return prefix;
+    }
+
+    public String longestCommonPrefix(String str1, String str2) {
+        int length = Math.min(str1.length(), str2.length());
+        int index = 0;//index求出str1和str2前index位相同
+        while (index < length && str1.charAt(index) == str2.charAt(index)) {
+            index++;
+        }
+        return str1.substring(0, index);//返回0，index位的str1
+    }
+}
+
+总的思路是创建第一个的string 然后（利用substring，charat，startwith等方法，若不等则用substring长度减一）依次和对面对比
